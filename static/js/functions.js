db66cfac9cf1106a39bcb97eb0f4e07d683333f1
Dave Graff <daveagraff@gmail.com>
2019-04-02 18:46:29 -0400

    Moved js out of main html file. Added basic shading to show availability

---
+++ static/js/functions.js
@@ -0,0 +1,176 @@
+//Call timing function
+setInterval(Function("timeIncrement();"), 1000);
+
+		//Initialize the two most important variables
+		var climateState = 0;
+		pointsCounter = 0;
+
+		//Help the climate & your points by clicking!
+		clickPower = 1;
+		updatePoints(0);
+		$("#climateState").text(climateState);
+		$("#mainIcon").click(function(evt){
+			updatePoints(clickPower);
+			$("#climateState").text(++climateState);
+			checkAvailable();
+		});
+
+		itemCount = new Array(7).fill(0);
+		itemEffectiveness = new Array(7).fill(1);
+
+		$("#itemsList tr").each(function(){
+			orig = $(this).children().first().children().first();
+			index = $(this).index();
+			//Set cost
+			orig.children().eq(1).children().eq(1).text(itemCost(index, itemCount[index]));
+			$(this).click(function(e){
+				orig = $(this).children().first().children().first();
+				index = $(this).index();
+				cost = itemCost(index, itemCount[index]);
+				if(cost <= pointsCounter){
+					checkUnavailable();
+					updatePoints(-(cost));
+					//Reset cost
+					orig.children().eq(1).children().eq(1).text(itemCost(index, ++itemCount[index]));
+					//Reset num owned
+					orig.children().eq(1).children().eq(0).text(itemCount[index]);
+				}
+				e.preventDefault();
+			});
+		});
+
+		var timePassed = 0;
+		function timeIncrement(){
+			climateState -= Math.pow(2, Math.floor(timePassed / 30));
+			if(climateState < -5000000)
+				document.location.href = 'LosePage.html';
+			if(climateState > 5000000)
+				document.location.href = 'WinPage.html';
+
+			for (var i = 0; i < itemCount.length; i++) {
+				gain = itemGain(i);
+				updatePoints(gain);
+				climateState += gain;
+				$("#climateState").text(climateState);
+			}
+
+			$("#climateState").text(climateState);
+			checkAvailable();
+			timePassed++;
+		}
+
+
+
+		function itemCost(index, numOwned){
+			return Math.ceil(Math.pow(2, index*3) * Math.pow(1.5, numOwned));
+		}
+
+		function itemGain(index){
+			return Math.ceil(Math.pow(2, 2 * index) * itemEffectiveness[index] * itemCount[index]);
+		}
+
+		function updatePoints(change) {
+			pointsCounter += change;
+			$("#pointsCounter").text(pointsCounter);
+		}
+
+		function handleUpgrade(upgradeIndex, change, upgradeId, cost){
+			if(cost <= pointsCounter){
+				checkUnavailable();
+				updatePoints(-(cost));
+				if(upgradeIndex == -1)
+					clickPower += change;
+				else
+					itemEffectiveness[upgradeIndex] += change;
+				$('#' + upgradeId).detach();
+			}
+		}
+		function move() {
+			var elem = document.getElementById("prog");
+			var width = 1;
+			var id = setInterval(frame, 10);
+			function frame() {
+				if (width >= 100) {
+					clearInterval(id);
+				} else {
+					width++;
+					if(climateState>0){
+						elem.style.paddingLeft = climateState/5000000 + '%';
+						elem.style.backgroundColor="green";
+					}
+					else{
+						elem.style.paddingRight = climateState/5000000 + '%';
+						elem.style.backgroundColor="red";
+					}
+				}
+			}
+		}
+
+		var coll = document.getElementsByClassName("collapsible_button");
+		var row = document.getElementsByClassName("side_collapsible");
+		var i;
+		for (i = 0; i < coll.length; i++) {
+			coll[i].addEventListener("click", function() {
+				this.classList.toggle("active");
+				var content = this.nextElementSibling;
+				if (content.style.maxHeight){
+					content.style.maxHeight = null;
+					content.style.padding="0px";
+					content.style.paddingBottom= "0px";
+				} else {
+					content.style.maxHeight = content.scrollHeight + "px";
+					content.style.padding= "10px";
+					content.style.paddingBottom= "50px";
+				}
+			});
+		}
+
+		var map = {}; // You could also use an array
+		document.addEventListener("keydown", cheatfunc);
+		document.addEventListener("keyup", cheatfunc);
+		function cheatfunc(e){
+		e = e || event; // to deal with IE
+		map[e.keyCode] = (e.type == 'keydown');
+		if(map[17] && map[18] && map[82]){
+			document.getElementById("mainIcon").src="../static/images/Ritesh.jpg"
+		}
+	}
+
+	function checkAvailable(){
+		$(".buttonDisabled").each(function(){
+			var cost = $(this).children().first().children().eq(0).children().eq(1).children().eq(1).text();
+			if(parseInt(cost) <= pointsCounter){
+				$(this).addClass("buttonEnabled");
+				$(this).removeClass("buttonDisabled");
+			}
+		});
+
+		$(".buttonDisabledUpgrade").each(function(){
+			var cost = $(this).children().eq(1).text();
+			cost = cost.replace(/[\$,\(\)]/g, "");
+			console.log(cost)
+			if(parseInt(cost) <= pointsCounter){
+				$(this).addClass("buttonEnabledUpgrade");
+				$(this).removeClass("buttonDisabledUpgrade");
+			}
+		});
+	}
+
+	function checkUnavailable(){
+		$(".buttonEnabled").each(function(){
+			var cost = $(this).children().first().children().eq(0).children().eq(1).children().eq(1).text();
+			if(parseInt(cost) > pointsCounter){
+				$(this).addClass("buttonDisabled");
+				$(this).removeClass("buttonEnabled");
+			}
+		});
+
+		$(".buttonEnabledUpgrade").each(function(){
+			var cost = $(this).children().eq(1).text();
+			cost = cost.replace(/[\$,\(\)]/g, "");
+			if(parseInt(cost) > pointsCounter){
+				$(this).addClass("buttonDisabledUpgrade");
+				$(this).removeClass("buttonEnabledUpgrade");
+			}
+		});
+	}
